02781101d1712116897862e1dc20637b5d655e09
Sean Wright <sean@seangwright.me>
2022-10-24 01:26:09 -0400

    build(sln): vscode config, nuget config, editorconfig, format Program.fs

--- src/Microsoft.eShopWeb.Web/Program.fs
+++ src/Microsoft.eShopWeb.Web/Program.fs
@@ -9,47 +9,47 @@ open System
 open Falco.Markup
 
 let getById =
-    fun (repository) -> fun (id: Guid) -> repository |> List.tryFind (fun x -> x.Id = id)
+  fun (repository) -> fun (id: Guid) -> repository |> List.tryFind (fun x -> x.Id = id)
 
 let getCatalogItemByIdFromRoute =
-    fun (route: RouteCollectionReader) ->
-        let getCatalogItem = getById catalogItems
-        route.TryGetGuid "id" |> Option.bind getCatalogItem
+  fun (route: RouteCollectionReader) ->
+    let getCatalogItem = getById catalogItems
+    route.TryGetGuid "id" |> Option.bind getCatalogItem
 
 let notFoundHandler: HttpHandler =
-    Response.withStatusCode 404 >> Response.ofPlainText "Not found"
+  Response.withStatusCode 404 >> Response.ofPlainText "Not found"
 
 let responseHandler =
-    fun (value) ->
-        match value with
-        | Some x -> Response.ofJson x
-        | None -> notFoundHandler
+  fun (value) ->
+    match value with
+    | Some x -> Response.ofJson x
+    | None -> notFoundHandler
 
 // ------------
 // Exception Handler
 // ------------
 let exceptionHandler: HttpHandler =
-    Response.withStatusCode 500 >> Response.ofPlainText "Server error"
+  Response.withStatusCode 500 >> Response.ofPlainText "Server error"
 
 let htmlHandler: HttpHandler =
-    let html =
-        Elem.html
-            [ Attr.lang "en" ]
-            [ Elem.head [] []
-              Elem.body [] [ Elem.h1 [] [ Text.raw "Welcome to the F# Shop!" ] ] ]
+  let html =
+    Elem.html
+      [ Attr.lang "en" ]
+      [ Elem.head [] []
+        Elem.body [] [ Elem.h1 [] [ Text.raw "Welcome to the F# Shop!" ] ] ]
 
-    Response.ofHtml html
+  Response.ofHtml html
 
 [<EntryPoint>]
 let main args =
-    webHost args {
-        use_if FalcoExtensions.IsDevelopment DeveloperExceptionPageExtensions.UseDeveloperExceptionPage
-        use_ifnot FalcoExtensions.IsDevelopment (FalcoExtensions.UseFalcoExceptionHandler exceptionHandler)
+  webHost args {
+    use_if FalcoExtensions.IsDevelopment DeveloperExceptionPageExtensions.UseDeveloperExceptionPage
+    use_ifnot FalcoExtensions.IsDevelopment (FalcoExtensions.UseFalcoExceptionHandler exceptionHandler)
 
-        endpoints
-            [ get "/" htmlHandler
+    endpoints
+      [ get "/" htmlHandler
 
-              get "/catalogItems/{id:guid}" (Request.mapRoute getCatalogItemByIdFromRoute responseHandler) ]
-    }
+        get "/catalogItems/{id:guid}" (Request.mapRoute getCatalogItemByIdFromRoute responseHandler) ]
+  }
 
-    0
+  0
