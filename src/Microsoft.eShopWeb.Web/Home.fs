5e4e00412d5fb5bdcba32f7d38a943c88b277623
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2022-10-27 21:35:14 -0400

    Add navbar (minus boostrap files)

--- src/Microsoft.eShopWeb.Web/Home.fs
+++ src/Microsoft.eShopWeb.Web/Home.fs
@@ -8,7 +8,38 @@ module Home =
 
   let head = ViewTemplate.head { Title = "Home"; Description = "" }
 
-  let body = ViewTemplate.body [ Elem.h1 [] [ Text.raw "Welcome to the F# Shop!!" ] ]
+  //Elem.h1 [] [ Text.raw "Welcome to the F# Shop!!" ] ]
+  let navBar =
+    Elem.header
+      [ Attr.class' "esh-app-header" ]
+      [ Elem.div
+          [ Attr.class' "container" ]
+          [ Elem.article
+              [ Attr.class' "row" ]
+              [ Elem.section
+                  [ Attr.class' "col-lg-7 col-md-6 col-xs-12" ]
+                  [ Elem.a [ Attr.href "/" ] [ Elem.img [ Attr.src "/images/brand.png"; Attr.alt "F# Shop On Web" ] ] ]
+                Elem.section
+                  [ Attr.class' "col-lg-4 col-md-5 col-xs-12" ]
+                  [ Elem.div
+                      [ Attr.class' "esh-identity" ]
+                      [ Elem.section
+                          [ Attr.class' "esh-identity-section" ]
+                          [ Elem.div
+                              [ Attr.class' "esh-identity-item" ]
+                              [ Elem.a
+                                  [ Attr.class' "esh-identity-name esh-identity-name--upper"
+                                    Attr.href "/Identity/Account/Login" ]
+                                  [ Text.raw "Login" ] ] ] ] ]
+                Elem.section
+                  [ Attr.class' "col-lg-1 col-xs-12" ]
+                  [ Elem.a
+                      [ Attr.class' "esh-basketstatus"; Attr.href "/Basket" ]
+                      [ Elem.div [ Attr.class' "esh-basketstatus-image" ] [ Elem.img [ Attr.src "/images/cart.png" ] ]
+                        Elem.div [ Attr.class' "esh-basketstatus-badge" ] [ Text.raw "0" ] ] ] ] ] ]
+
+  let body =
+    ViewTemplate.body [ Elem.div [ Attr.class' "esh-app-wrapper" ] [ navBar ] ]
 
   let page = ViewTemplate.layout head body
 
