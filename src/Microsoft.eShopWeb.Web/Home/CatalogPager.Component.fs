8989f91ac05e9c9977919e9de449b6e38159f162
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2022-11-05 09:28:27 -0400

    Refactoring the CatalogPager component to have dynamic behavior

--- src/Microsoft.eShopWeb.Web/Home/CatalogPager.Component.fs
+++ src/Microsoft.eShopWeb.Web/Home/CatalogPager.Component.fs
@@ -3,8 +3,27 @@ namespace Microsoft.eShopWeb.Web.Home
 open Falco.Markup.Attr
 open Falco.Markup.Elem
 open Falco.Markup.Text
+open Microsoft.eShopWeb.Web.Domain
 
 module CatalogPagerComponent =
+  module private Template =
+    let currentPage = 1
+    let pageSize = 10
+    let totalPages = (catalogItems.Length / pageSize) + 1
+    let morePagesAvailable = catalogItems.Length > pageSize
+    let hasPreviousPage = currentPage > 1
+    let hasNextPage = currentPage < totalPages
+
+    let totalItemsOnPage =
+      match morePagesAvailable with
+      | true -> pageSize
+      | false -> catalogItems.Length
+
+    let currentItemIndex = (pageSize * (currentPage - 1)) + 1
+
+    let pagingText =
+      sprintf "Showing %d of %d products - Page %d - %d" currentItemIndex totalItemsOnPage currentPage totalPages
+
   let cmpt =
     div
       [ class' "esh-pager" ]
@@ -20,9 +39,7 @@ module CatalogPagerComponent =
                           [ class' "esh-pager-item-left esh-pager-item--navigable esh-pager-item is-disabled"
                             href "/?pageId=-1" ]
                           [ raw "Previous" ] ]
-                    div
-                      [ class' "col-md-8 col-xs-12" ]
-                      [ span [ class' "esh-pager-item" ] [ raw "Showing 3 of 3 products - Page 1 - 1" ] ]
+                    div [ class' "col-md-8 col-xs-12" ] [ span [ class' "esh-pager-item" ] [ raw Template.pagingText ] ]
                     div
                       [ class' "col-md-2 col-xs-12" ]
                       [ a
